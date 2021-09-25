f6dfcf40078d927fe9f0d552c4662d07612f7acf
miles <miles1618@gmail.com>
2021-09-24 19:31:55 -0500

    Harvest, packed, forsale

--- project-6/contracts/coffeebase/SupplyChain.sol
+++ project-6/contracts/coffeebase/SupplyChain.sol
@@ -183,7 +183,7 @@ contract SupplyChain {
     // Define a function 'processtItem' that allows a farmer to mark an item 'Processed'
     function processItem(uint _upc) public
         // Call modifier to check if upc has passed previous supply chain stage
-        harvestItem(_upc)
+        harvested(_upc)
         // Call modifier to verify caller of this function
         verifyCaller(items[_upc].ownerID)
     {
@@ -196,27 +196,28 @@ contract SupplyChain {
     // Define a function 'packItem' that allows a farmer to mark an item 'Packed'
     function packItem(uint _upc) public
         // Call modifier to check if upc has passed previous supply chain stage
-
+        processed(_upc)
         // Call modifier to verify caller of this function
-
+        verifyCaller(items[_upc].ownerID)
     {
         // Update the appropriate fields
-
+        items[_upc].itemState = State.Packed;
         // Emit the appropriate event
-
+        Packed(_upc);
     }
 
     // Define a function 'sellItem' that allows a farmer to mark an item 'ForSale'
     function sellItem(uint _upc, uint _price) public
         // Call modifier to check if upc has passed previous supply chain stage
-
+        packed(_upc)
         // Call modifier to verify caller of this function
-
+        verifyCaller(items[_upc].ownerID)
     {
         // Update the appropriate fields
-
+        items[_upc].itemState = State.ForSale;
+        items[_upc].productPrice = _price;
         // Emit the appropriate event
-
+        emit ForSale(_upc);
     }
 
     // Define a function 'buyItem' that allows the disributor to mark an item 'Sold'
